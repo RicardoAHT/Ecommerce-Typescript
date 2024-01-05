3ffcc21efabe986c98f437c5534521d4b3c2a88b
Ricardo <rickxd007@gmail.com>
2024-01-05 18:12:49 -0500

    Añado el boton con la funcionalidad para comprar el carrito

--- src/components/CartPage/CartTotal.tsx
+++ src/components/CartPage/CartTotal.tsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { RootState } from '../../store'
+import useFetchCart from '../../hooks/useFetchCart'
 
 const CartTotal: React.FC = () => {
 
+  const {buyCart} = useFetchCart()
   const totalValue = useSelector((store: RootState) => store.cartValue)
+
+  const handleBuy = () => {
+    buyCart("/purchases")
+  }
 
   return (
     <div className='cartTotal'>
       <div className='cartTotal__container'>
         <hr className='cartTotal__hr' />
         <span className='cartTotal__span'>Total: ${totalValue}</span>
+        <button onClick={handleBuy}>Buy</button>
       </div>
     </div>
   )
