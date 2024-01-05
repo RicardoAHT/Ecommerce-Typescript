169263984f59527026199bcd54d26fa812b372ea
Ricardo <rickxd007@gmail.com>
2024-01-05 08:39:21 -0500

    Implemento logica en los botones para actualizar el valor del estado global de precio total del carrito cada vez que aumento o disminuyo cantidades de los productos

--- src/components/CartPage/CartCard.tsx
+++ src/components/CartPage/CartCard.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import useFetchCart from '../../hooks/useFetchCart';
 import { setCartCounterSlice } from '../../store/slices/cartCounter.slice';
 import { useDispatch, useSelector } from 'react-redux';
+import { setCartValueSlice } from '../../store/slices/cartValue.slice';
 
 interface CartCardProps {
 product:{
@@ -39,15 +40,22 @@ const CartCard: React.FC<CartCardProps> = ({product, setDeleteApiEffect, deleteA
   const [ infoApi, getApi, hasError, loading, postApi, deleteApi ] = useFetchCart()
 
   const id = product.id
+  const productTotal = quantity * product.product.price
   
   let cartCounter = useSelector(store => store.cartCounter)
   const dispatch = useDispatch()
+  let totalValue = useSelector(store => store.cartValue)
+  
+  
+  //dispatch(setCartValueSlice())
 
   const handlePlusQuantity = () => {
     let counter = quantity
     setQuantity(counter += 1)
     cartCounter += 1
     dispatch(setCartCounterSlice(cartCounter))
+    let value = totalValue + parseInt(product.product.price)
+    dispatch(setCartValueSlice(value))
   }
 
   const handleMinusQuantity = () => {
@@ -56,6 +64,8 @@ const CartCard: React.FC<CartCardProps> = ({product, setDeleteApiEffect, deleteA
       setQuantity(counter -= 1)
       cartCounter -= 1
       dispatch(setCartCounterSlice(cartCounter))
+      let value = totalValue - parseInt(product.product.price)
+      dispatch(setCartValueSlice(value))
     }
   }
 
@@ -63,10 +73,6 @@ const CartCard: React.FC<CartCardProps> = ({product, setDeleteApiEffect, deleteA
     deleteApi("/cart", id)
     setDeleteApiEffect(!deleteApiEffect)
   }
-
-  useEffect(() => {
-    
-  }, [loading])
   
   return (
     <article className='cartCard'>
@@ -80,6 +86,7 @@ const CartCard: React.FC<CartCardProps> = ({product, setDeleteApiEffect, deleteA
           <span> {quantity} </span>
         <button onClick={handlePlusQuantity}> + </button>
         <button onClick={handleDelete}>Delete</button>
+        <p>Total {productTotal}</p>
       </div>
     </article>
   )
