b1ae878246d2e4239f18e018fa33a3825c14843b
Ricardo <rickxd007@gmail.com>
2024-01-05 19:30:22 -0500

    Organizo la informacion que voy a traer de la api y nombro clases para dar estilos como en CartCard

--- src/components/PurchasesPage/PurchasesCard.tsx
+++ src/components/PurchasesPage/PurchasesCard.tsx
@@ -1,22 +1,60 @@
 import React from 'react'
 
 interface Purchase {
+  product:{
+    id:number
+    title: string
+    price: string
+    images:[
+      {
+        url: string
+      }
+    ]
+  }
   id: number;
   productId: number;
   createdAt: string;
 }
 
 const PurchasesCard: React.FC<{ purchase: Purchase }> = ({purchase}) => {
+  const product = purchase.product
+  console.log(purchase)
   return (
-    <div>
-      <ul>
-        <li>{purchase.id}</li>
-        <li>{purchase.productId}</li>
-        <li>{purchase.createdAt}</li>
-        <li>Quantity</li>
-        <li>Price</li>
-      </ul>
-    </div>
+    <article className='cartCard'>
+      <div className='cartCard__container__img'>
+        <img src={
+          (product === null)
+            ? ""
+            : product.images[0].url       
+          } 
+          alt="Imagen Producto"
+          className='cartCard__img'
+        />
+      </div>
+      <div className='cartCard__container'>
+        <ul className='cartCard__ul'>
+          <li className='cartCard__ul__li'>
+            {
+              (product === null)
+                ? "No hay informacion del producto"
+                : product.title
+            }
+          </li>
+          <li>Quantity: 
+            {purchase.productId}</li>
+          <li>Price: 
+            {
+              (product === null)
+                ? "---"
+                : product.price
+            }
+
+          </li>
+          <li>Fecha: {purchase.createdAt}</li>
+
+        </ul>
+      </div>
+    </article>
   )
 }
 
