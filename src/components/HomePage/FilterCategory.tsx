0bff592f01f138b24737c6f4a1ebea6ad2ddae19
Ricardo <rickxd007@gmail.com>
2024-01-08 11:02:44 -0500

    Realizo la logica para filtrar la busqueda de productos por categoria y separo la busqueda por titulo en otro componente

--- src/components/HomePage/FilterCategory.tsx
+++ src/components/HomePage/FilterCategory.tsx
@@ -1,18 +1,48 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import useFetchToken from '../../hooks/useFetchToken'
+import { setSearchSlice } from '../../store/slices/search.slice';
+import { useDispatch } from 'react-redux';
+import FilterByTittle from './FilterByTittle';
+
+interface Category{
+  createdAt: string;
+  id: number;
+  name: string;
+  updateAt: string;
+}
 
 const FilterCategory: React.FC = () => {
+
+  const [categories, getCategories] = useFetchToken()
+  const dispatch = useDispatch()
+  useEffect(() => {
+    getCategories("/categories")
+  }, [])
+  //console.log(categories)
+
+  const handleCategory = (event: React.FormEvent<HTMLFormElement>) => {
+    dispatch(setSearchSlice(`?categoryId=${event.target.value}`))
+  }
+
   return (
     <div className='filterByCategory'>
       <h3>Filter Category</h3>
-      <form >
+      <form onChange={handleCategory} >
         <select name="" id="">
-          <option value="">----</option>
+          <option value="">all</option>
+          {
+            categories?.map(category => (
+              <option 
+                key={category.id} 
+                value={category.id}
+              >
+                {category.name}
+              </option>
+            ))
+          }
         </select>
-        <div>
-          <input type="text" name="" id="" />
-          <button>Search</button>
-        </div>
       </form>
+      <FilterByTittle/>
     </div>
   )
 }
