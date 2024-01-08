cddeedc7b166b52b3a14b2f695d7df591444a158
Ricardo <rickxd007@gmail.com>
2024-01-08 11:05:53 -0500

    Paso los filtros a ProductList

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import NavBar from '../components/shared/NavBar'
 import Header from '../components/shared/Header'
 import ProductList from '../components/HomePage/ProductList'
-import FilterByPrice from '../components/HomePage/FilterByPrice'
-import FilterCategory from '../components/HomePage/FilterCategory'
 
 const HomePage: React.FC = () => {
 
@@ -12,10 +10,6 @@ const HomePage: React.FC = () => {
     <div>
       <NavBar/> {/*Renderizo eñ navegador */}
       <Header/>
-      <div className='homepage__Container'>
-        <FilterByPrice/>
-        <FilterCategory/>
-      </div>
       <main>
         <ProductList/> {/*Lista de Productos para agregar al estado global del carrito */}
       </main>
