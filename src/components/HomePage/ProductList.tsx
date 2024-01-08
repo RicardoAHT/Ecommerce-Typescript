87e8f5ef43e838f916cc1f33fc8db8387d2b666d
Ricardo <rickxd007@gmail.com>
2024-01-08 11:04:05 -0500

    Adecuo la constante de la url para hacerla dinamica con los filtros y que se rrenderice cada vez que haya un cambio en el valor de la busqueda

--- src/components/HomePage/ProductList.tsx
+++ src/components/HomePage/ProductList.tsx
@@ -2,15 +2,23 @@ import React, { useEffect } from 'react'
 import useFetch from '../../hooks/useFetch'
 import ProductCard from './ProductCard'
 import Loading from '../shared/Loading'
+import FilterByPrice from './FilterByPrice'
+import FilterCategory from './FilterCategory'
+import { useSelector } from 'react-redux'
+import { RootState } from '../../store'
 
 const ProductList: React.FC = () => {
 
-    const url = "https://ecommercebackendbyrick.onrender.com/products"
+    const search = useSelector((store: RootState) => store.search)
+
+    const url = `https://ecommercebackendbyrick.onrender.com/products${search}`
+    const url2 = `https://ecommercebackendbyrick.onrender.com/products?title=${""}`
     const [products, getProducts, hasError, loading] = useFetch(url)
   
     useEffect(() => {
       getProducts()
-    }, [])
+    }, [search])
+    //console.log(products)
 
     if (loading) {
       return (
@@ -24,20 +32,26 @@ const ProductList: React.FC = () => {
       return <p>Error al obtener los datos.</p>;
     }
   
-    console.log(products)
-
-
   return (
-    <section className='productList'>
-      {
-        products?.map(product => (
-          <ProductCard
+    <>
+      <div className='homepage__Container'>
+        <FilterByPrice
+        />
+        <FilterCategory
+          products={products}
+        />
+      </div>
+      <section className='productList'>
+        {
+          products?.map(product => (
+            <ProductCard
             key={product.id}
             product={product}
-          />
-        ))
-      }
-    </section>
+            />
+          ))
+        }
+      </section>
+    </>
   )
 }
 
