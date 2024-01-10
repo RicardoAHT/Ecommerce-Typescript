93cc010674a7bf7e530e1f223fb54e959c213814
Ricardo <rickxd007@gmail.com>
2024-01-09 20:50:04 -0500

    Agrego el estado de cartRefresh a la page para poder pasarlo por props a sus hijos unicamente

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from '../components/shared/NavBar'
 import Header from '../components/shared/Header'
 import CartList from '../components/CartPage/CartList'
 import CartTotal from '../components/CartPage/CartTotal'
 
 const CartPage: React.FC = () => {
+  const [cartRefresh, setCartRefresh] = useState(false)
+
   return (
     <>
       <NavBar/>
       <Header/>
-      <CartList/>
-      <CartTotal/>
+      <CartList
+        setCartRefresh={setCartRefresh}
+        cartRefresh={cartRefresh}
+      />
+      <CartTotal
+        setCartRefresh={setCartRefresh}
+        cartRefresh={cartRefresh}
+      />
     </>
   )
 }
